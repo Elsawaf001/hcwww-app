82354303ffe2b4000a17ff5f305b1ae78bfb78b5
a.elsawaf <elsawaf001@gmail.com>
2023-05-22 14:16:55 +0200

    done

--- src/app/components/activity/activity.component.ts
+++ src/app/components/activity/activity.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Activity} from "../../model/activity";
+import {ActivityService} from "../../service/activity.service";
+import {Subscription} from "rxjs";
+import {HttpErrorResponse} from "@angular/common/http";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-activity',
   templateUrl: './activity.component.html',
   styleUrls: ['./activity.component.css']
 })
-export class ActivityComponent {
+export class ActivityComponent implements OnInit{
+  showTable : boolean = false;
+  selectedActivity : Activity | undefined ;
+
+
+
+
+  activityDataSource : Activity[] = [];
+  private subscriptions:Subscription[] = [];
+
+
+
+  constructor(private activityService : ActivityService , private toastrService : ToastrService) {
+  }
+
+
+
+  getAllActivities(){
+    this.subscriptions.push(
+      this.activityService.getAllActivities().subscribe(
+        {
+          next : (response : Activity[]) => {
+            response.forEach(activity => this.activityDataSource.push(activity))
+            localStorage.setItem('activities' , JSON.stringify(response))
+            this.toastrService.success("تم تحميل الانشطة بنجاح")
+          } ,
+          error : (err : HttpErrorResponse) => {
+            this.toastrService.error(err.error.message , "فشل تحميل الانشطة")
+          }
+        }
+      )
+    )
+  }
+
+  ngOnInit(): void {
+    this.getAllActivities();
+  }
 
 }
--- src/app/components/licence-area/licence-area.component.ts
+++ src/app/components/licence-area/licence-area.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Activity} from "../../model/activity";
+import {Subscription} from "rxjs";
+import {LicenceArea} from "../../model/licence-area";
+import {LicenceAreaService} from "../../service/licence-area.service";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-licence-area',
   templateUrl: './licence-area.component.html',
   styleUrls: ['./licence-area.component.css']
 })
-export class LicenceAreaComponent {
+export class LicenceAreaComponent implements OnInit{
+  showTable : boolean = false;
+  selectedLicense : LicenceArea | undefined ;
+
+
+
+  licences : LicenceArea[] = [];
+
+  private subscriptions:Subscription[] = [];
+
+
+constructor(private service : LicenceAreaService , private toasterService : ToastrService) {
+}
+
+
+getAllLicences(){
+  this.subscriptions.push(
+    this.service.getAll().subscribe(
+      {
+        next : (response : LicenceArea[]) =>{
+          response.forEach(area => this.licences.push(area))
+          console.log(response)
+
+        } ,
+        error : err => this.toasterService.error(err.error.message)
+      }
+    )
+  )
+}
+
+
+  ngOnInit(): void {
+  this.getAllLicences()
+  }
 
 }
